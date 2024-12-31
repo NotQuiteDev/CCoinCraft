de4df3a71ade617860423f819486f284ea8da63e
SkylerWhiteYoYo <dlcksgur15@naver.com>
2024-12-31 12:55:45 +0900

    버그 방금거 수정 안돼서 다시 수정해서 잘 작동하고
    문제점 발견:
    광물 캔거만 btc_history에 나와야하는데 sell한것도 뜸 이건 문제

--- src/main/java/org/lch/cCoinCraft/database/BtcHistoryDAO.java
+++ src/main/java/org/lch/cCoinCraft/database/BtcHistoryDAO.java
@@ -18,16 +18,16 @@ public class BtcHistoryDAO {
      * BTC 거래 내역 INSERT
      * @param uuid      플레이어 UUID
      * @param nickname  닉네임
-     * @param amount    거래 수량 (양수면 구매, 음수면 판매)
+     * @param amount_btc    거래 수량 (양수면 구매, 음수면 판매)
      * @param reason    "BUY_BTC" or "SELL_BTC"
      */
-    public void insertHistory(String uuid, String nickname, double amount, String reason) {
+    public void insertHistory(String uuid, String nickname, double amount_btc, String reason) {
         queryQueue.addTask(new QueryTask(databaseManager, (Connection conn) -> {
-            String sql = "INSERT INTO btc_history (uuid, nickname, amount, reason) VALUES (?, ?, ?, ?)";
+            String sql = "INSERT INTO btc_history (uuid, nickname, amount_btc, reason) VALUES (?, ?, ?, ?)";
             try (PreparedStatement ps = conn.prepareStatement(sql)) {
                 ps.setString(1, uuid);
                 ps.setString(2, nickname);
-                ps.setDouble(3, amount);
+                ps.setDouble(3, amount_btc);
                 ps.setString(4, reason);
                 ps.executeUpdate();
             } catch (SQLException e) {
